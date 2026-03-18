b23c78bcceb05aa5e7f7c576b4ba67921f64e457
EnceladeCandy <noe.dia311@gmail.com>
2026-03-18 10:24:02 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="astro-peek",
+    version="0.1",
+    description="Package to investigate the learned latent space of ML models",
+    author="Gabriel Missael Barco, Salma Salhi, Noé Dia",
+    packages=["astro-peek"],
+    install_requires=[],
+)
